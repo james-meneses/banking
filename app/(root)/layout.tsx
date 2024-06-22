1c185a6922615cebed530be73ae1a1e061dc9f9e
James Meneses <james.meneseswk@gmail.com>
2024-06-22 20:36:24 -0300

    Implements mobile nav sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
 
 export default function RootLayout({
   children,
@@ -15,10 +17,15 @@ export default function RootLayout({
       <Sidebar user={loggedIn} />
 
       <div className="flex size-full flex-col">
-        <div className="root-layout"></div>
-      </div>
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
 
-      {children}
+        {children}
+      </div>
     </main>
   );
 }
--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -17,12 +17,12 @@ const TotalBalanceBox = ({
       <div className="flex flex-col gap-6">
         <h2 className="header-2">Bank Accounts: {totalBanks}</h2>
 
-        <div className="fle flex-col gap-2">
+        <div className="flex flex-col gap-2">
           <p className="total-balance-label">Total Current Balance</p>
 
-          <p className="total-balance-amount flex-center gap-2">
+          <div className="total-balance-amount flex-center gap-2">
             <AnimatedCounter amount={totalCurrentBalance} />
-          </p>
+          </div>
         </div>
       </div>
     </section>
