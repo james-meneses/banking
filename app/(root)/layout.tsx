efaf2fd65de1033250ed84d0dbf00995af90f06f
James Meneses <james.meneseswk@gmail.com>
2024-06-22 16:27:44 -0300

    implements sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,23 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {
+    firstName: "James",
+    lastName: "Meneses",
+  };
+
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout"></div>
+      </div>
+
       {children}
     </main>
   );
