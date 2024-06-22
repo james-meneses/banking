1efe357178ce3219daff1d175f614483675b8e35
James Meneses <james.meneseswk@gmail.com>
2024-06-22 15:29:56 -0300

    Makes the Home UI

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div>
+      <CountUp decimal="," prefix="R$ " decimals={2} end={amount} />
+    </div>
+  );
+};
+
+export default AnimatedCounter;
