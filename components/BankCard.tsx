c1305aeecd57c2add78ccd34a64174d3d51b4ed8
James Meneses <james.meneseswk@gmail.com>
2024-06-22 22:08:44 -0300

    Implements Bank Cards

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -1,4 +1,5 @@
 import { formatAmount } from "@/lib/utils";
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
@@ -23,9 +24,35 @@ const BankCard = ({
           <article className="flex flex-col gap-2">
             <div className="flex justify-between">
               <h1 className="text-12 font-semibold text-white">{userName}</h1>
+              <h2 className="text-12 font-semibold text-white">
+                &nbsp; ** / **
+              </h2>
             </div>
+            <p className="text-14 font-semibold tracking-[1.1px] text-white">
+              ●●●● ●●●● ●●●●{" "}
+              <span className="text-16">{account.mask || "0000"}</span>
+            </p>
           </article>
         </div>
+
+        <div className="bank-card_icon">
+          <Image src="/icons/Paypass.svg" width={20} height={24} alt="pay" />
+          <Image
+            src="/icons/mastercard.svg"
+            width={45}
+            height={32}
+            alt="mastercard"
+            className="ml-5"
+          />
+        </div>
+
+        <Image
+          src="/icons/lines.png"
+          width={316}
+          height={190}
+          alt="Lines"
+          className="absolute top-0 right-0"
+        />
       </Link>
     </div>
   );
